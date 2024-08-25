48c13fd4550a5a0e8166e344606a38d24083c8f1
Shreya Shrivastava <shrivastavashreya32@gmail.com>
2024-08-25 21:36:16 +0530

    3rd try

--- myapp/apps.py
+++ myapp/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
-    #git add . ,git commit -m "Your descriptive commit message" ,git push origin branch-name
+    #git add . ,git commit -m "Your descriptive commit message" ,git push origin main
